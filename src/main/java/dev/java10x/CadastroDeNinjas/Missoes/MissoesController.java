574217950ff6287d8fa1be034b2a3d2b13c77407
Diego Galante <diegogalante812@gmail.com>
2026-02-18 22:26:26 -0300

    feat: Controller e Service de missoes feito com CRUD menos o metodo PUT que precisa de conserto

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,43 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+   private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    // Criar Missao
+    @PostMapping("/criar")
+    public MissoesModel criarMissao(@RequestBody MissoesModel missoesModel){
+        return missoesService.criarMissao(missoesModel);
+    }
+
+    // Listar todas as missoes
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissao(){
+        return missoesService.listarMissao();
+    }
+
+
+    // Listar missoes por id
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissaoPorId(id);
+    }
+
+    // Alterar missoes
+
+    // Deletar missoes
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissaoPorId(@PathVariable Long id){
+        missoesService.deletarMissaoPorId(id);
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,45 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Criar Missao
+    public MissoesModel criarMissao(MissoesModel missoesModel){
+        return missoesRepository.save(missoesModel);
+    }
+
+    // Listar todas as missoes
+    public List<MissoesModel> listarMissao(){
+        return missoesRepository.findAll();
+    }
+
+    // Listar missoes por id
+    public MissoesModel listarMissaoPorId(Long id){
+        Optional<MissoesModel> missaoPorId = missoesRepository.findById(id);
+        return missaoPorId.orElse(null);
+    }
+
+
+    // Alterar missoes
+
+
+    // Deletar missoes
+    public void deletarMissaoPorId(Long id){
+        missoesRepository.deleteById(id);
+    }
 }
